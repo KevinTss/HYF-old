3d4d5b9a88dc0f0f529eac3873f39021901bb318
Kevin Tassi <kevin@fishtripr.com>
2019-04-14 20:37:00 +0200

    step 2 - add array of objects

--- 06-NodeJS/Labs/01-lodash-handling/index.js
+++ 06-NodeJS/Labs/01-lodash-handling/index.js
@@ -1,1 +1,9 @@
 const _ = required('lodash');
+
+const people = [
+  { name: "John", age: 77 },
+  { name: "Jane", age: 23 },
+  { name: "Eliot", age: 42 },
+  { name: "Brice", age: 65 },
+  { name: "Lizzy", age: 28 }
+]
