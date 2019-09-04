9109beea1c3e347a819110ff88dcbf600d313a1c
Kevin Tassi <kevin.tassi@gmail.com>
2019-09-04 08:46:09 +0200

    Add example Olivia

---
+++ 00-Extra-classes/Olivia/test-vue/test-app/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <button id="test"></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  beforeCreate: function() {
+    console.log('before create')
+    const a = document.getElementById('test')
+    console.log(a)
+  },
+  created() {
+    console.log('created')
+    const a = document.getElementById('test')
+    console.log(a)
+  },
+  beforeMount() {
+    console.log('before mount')
+    const a = document.getElementById('test')
+    console.log(a)
+  },
+  mounted() {
+    console.log('mounted')
+    const a = document.getElementById('test')
+    console.log(a)
+  },
+  beforeUpdate() {
+    console.log('before up')
+  },
+  updated() {
+    console.log('u')
+  },
+  beforeDestroy() {
+    console.log('dest')
+  },
+  destroyed() {
+    console.log('destroyed')
+  }
+}
+</script>
